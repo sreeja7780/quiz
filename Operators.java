99f4b4e2cacbfeae2218101a594ba88e23ceb8b9
sree7780 <saisreejapatluri@gmail.com>
2024-12-12 11:45:58 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,13 @@
+class Operators
+  {
+    public static void main(String[] args)
+    {
+      int a=20;
+      int b=5;
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a×b);
+      System.out.println(a/b);
+      System.out.println (a%b);
+    }
+  }
